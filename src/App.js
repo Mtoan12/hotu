f38af7c86648f9b2b10c3506a6011e04c17f8c7e
mtoan12 <20120215@student.hcmus.edu.vn>
2023-10-03 18:23:32 +0700

    flip

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <main>
-                <div class="flip-card-container">
-                    <div class="flip-card">
-                        <div class="card-front">
-                            <figure>
-                                <img src="/front.jpg" alt="" />
-                            </figure>
-                        </div>
-                        <div class="card-back">
-                            <figure>
-                                <img src="/back.jpg" alt="" />
-                            </figure>
-                        </div>
-                    </div>
+        <main class="flip-card-container">
+            <div class="flip-card">
+                <div class="card-front">
+                    <figure>
+                        <picture>
+                            <source media="(max-width: 800px)" srcset="/images/front-mobile.jpg" />
+                            <img src="/images/front.jpg" alt="" />
+                        </picture>
+                    </figure>
                 </div>
-            </main>
-        </div>
+                <div class="card-back">
+                    <figure>
+                        <picture>
+                            <source media="(max-width: 800px)" srcset="/images/back-mobile.jpg" />
+                            <img src="/images/back.png" alt="" />
+                        </picture>
+                    </figure>
+                </div>
+            </div>
+        </main>
     );
 }
 
